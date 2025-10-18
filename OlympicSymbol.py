76d1c84edab809550e2df424a80fc26694e8d1cb
FARKHOD ABDUKODIROV <abdukodirovfarkhod@gmail.com>
2025-10-18 13:55:30 +0900

    Create OlympicSymbol.py to draw Olympic Rings
    This script uses the turtle graphics library to draw the Olympic Rings logo with five colored circles.

---
+++ OlympicSymbol.py
@@ -0,0 +1,37 @@
+# Drawing the Olympic Rings Logo
+import turtle
+
+s = turtle.Screen()
+t = turtle.Turtle() # 'turtle' object
+
+t.color("blue")
+t.penup()
+t.goto(-110, -25)
+t.pendown()
+t.circle(45)
+
+t.color("black")
+t.penup()
+t.goto(0, -25)
+t.pendown()
+t.circle(45)
+
+t.color("red")
+t.penup()
+t.goto(110, -25)
+t.pendown()
+t.circle(45)
+
+t.color("brown")
+t.penup()
+t.goto(-55, -75)
+t.pendown()
+t.circle(45)
+
+t.color("green")
+t.penup()
+t.goto(55, -75)
+t.pendown()
+t.circle(45)
+
+s.mainloop()
